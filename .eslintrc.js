8c0ff88ed7e2457cdd1d9e0b47a34cdd873963da
Aarni Koskela <akx@iki.fi>
2019-11-12 10:23:08 +0200

    Add ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "commonjs": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "sketch"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
